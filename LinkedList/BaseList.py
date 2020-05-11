1ef4379015d02823c9f81339ea41cd04b0294ab8
gaurav0535 <gaurav0535@hotmail.com>
2020-05-11 12:22:27 +0530

    Recursion

--- LinkedList/BaseList.py
+++ LinkedList/BaseList.py
@@ -1,26 +1,49 @@
 class Node:
-    def __init__(self,data = None):
-        self.data = data
+    def __init__(self,data):
         self.next = None
+        self.data = data
 
-class SlinkedList:
+class SLinkedList:
     def __init__(self):
         self.head = None
 
-list1 = SlinkedList()
-list1.head = Node("Mon")
-e2 = Node("Tue")
-e3 = Node("Wed")
-e4 = Node("Thursday")
-e5 = Node("Friday")
+    def trav(self):
 
-#Link first Node to Second node
+        printval = self.head
+        if(printval):
 
-list1.head.next = e2
-e2.next = e3
-e3.next = e4
-e4.next = e5
+            if(printval.next == None):
+                print(printval.data)
+            else:
+                while(printval != None):
+                    print(printval.data)
+                    printval = printval.next
 
+        else:
+            print("Nothing is in linked list ")
+
+
+l = SLinkedList()
+l.head = Node(1)
+n = Node(2)
+n1 = Node(3)
+n2 = Node(3)
+n3 = Node(3)
+n4 = Node(3)
+l.head.next = n
+n.next = n1
+n1.next = n2
+n2.next = n3
+n3.next = n4
+
+l1 = SLinkedList()
+l1.head = Node(2)
+l1.trav()
+
+print("Before big one")
+l.trav()
+print("after last")
+SLinkedList().trav()
 
 
 
