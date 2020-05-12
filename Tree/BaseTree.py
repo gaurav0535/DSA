ae44a0f6b438980a49425a617ccf54f8c00dc097
gaurav0535 <gaurav0535@hotmail.com>
2020-05-12 13:46:01 +0530

    Binary Tree

--- Tree/BaseTree.py
+++ Tree/BaseTree.py
@@ -1,14 +1,48 @@
 class Node:
-    def __init__(self,key):
+    def __init__(self, data):
         self.left = None
         self.right = None
-        self.val = key
+        self.key = data
 
+
+def inOrder(root):
+    if root is None:
+        return
+
+    inOrder(root.left)
+    print(root.key)
+    inOrder(root.right)
+
+
+def preOrder(root):
+    if root is None:
+        return
+    print(root.key)
+    preOrder(root.left)
+    preOrder(root.right)
+
+def postOrder(root):
+    if root is None:
+        return
+
+    postOrder(root.left)
+    postOrder(root.right)
+    print(root.key)
 
 root = Node(1)
 root.left = Node(2)
 root.right = Node(3)
-root.left.left  = Node(4)
+root.right.left = Node(6)
+root.right.right = Node(7)
 
+root.left.left = Node(4)
+root.left.right = Node(5)
 
+print("In Order")
+inOrder(root)
 
+print("Pre Order")
+preOrder(root)
+
+print("Post Order")
+postOrder(root)
